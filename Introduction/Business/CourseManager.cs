d6438ea21e5532039296e6f5fcfe9f5ecfbb29f1
Batuhan Topçu <topcubatuhan00@hotmail.com>
2024-02-04 23:04:07 +0300

    first day

---
+++ Introduction/Business/CourseManager.cs
@@ -0,0 +1,29 @@
+﻿using Introduction.Entities;
+
+namespace Introduction.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[2];
+    public CourseManager()
+    {
+        Course course = new Course();
+        course.Id = 1;
+        course.Name = "C#";
+        course.Description = ".Net 8";
+        course.Price = 0;
+
+        Course course1 = new Course();
+        course1.Id = 2;
+        course1.Name = "Java";
+        course1.Description = "Eclipse";
+        course1.Price = 100;
+
+        courses[0] = course;
+        courses[1] = course1;
+    }
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Introduction/Program.cs
@@ -0,0 +1,40 @@
+﻿// ch-1
+
+using Introduction.Business;
+using Introduction.Entities;
+
+Console.WriteLine("Hello World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 10000;
+
+bool isAuthenticated = true;
+Console.WriteLine(message1);
+
+// ch-2
+
+if (isAuthenticated)
+    Console.WriteLine("Hoş geldin batu");
+else
+    Console.WriteLine("Giriş Yap");
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4" };
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new();
+var courses = courseManager.GetAll();
+
+for (int i = 0; i <courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Id);
+    Console.WriteLine(courses[i].Name);
+    Console.WriteLine(courses[i].Description);
+    Console.WriteLine(courses[i].Price);
+}
+
+Console.WriteLine("kod bitti");
