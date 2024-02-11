3797a5d0221895b7c4077c6cace0e32fa6873e1b
Batuhan Topçu <topcubatuhan00@hotmail.com>
2024-02-11 20:41:27 +0300

    hw-2

---
+++ HW2/Business/Concrete/PersonManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PersonManager : IApplicantService
+{
+	public void ApplyForMask(Person person)
+	{
+
+	}
+
+	public List<Person> GetList()
+	{
+		return null;
+	}
+
+	public bool CheckPerson(Person person)
+	{
+		//KPS->kimlik paylaşım sistemi
+		KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+		return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest
+			(new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, Ad: person.FirstName, Soyad: person.LastName, DogumYili: person.DataOfBirthYear))).Result.Body.TCKimlikNoDogrulaResult;
+	}
+
+}
---
+++ HW2/Workaround/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+	public string ad = "Engin";
+	public string soyad = "Demiroğ";
+	public int dogumYili = 1985;
+	public long tcNo = 123456789;
+
+	public string Ad { get; set; }
+	public string Soyad { get; set; }
+	public int DogumYili { get; set; }
+	public long TcNo { get; set; }
+}
